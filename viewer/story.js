5b6c2d3d7c347e035b9be85d95266d4e15813692
Jose.Glas <48224956+JoseGlasOLX@users.noreply.github.com>
2019-07-19 16:28:24 -0300

    primer commit
    subida de archivos

---
+++ viewer/story.js
@@ -0,0 +1,930 @@
+var story = {
+"docName": "ad-details",
+"docPath": "P_P_P",
+"docVersion": "V_V_V",
+"hasRetina": true,
+"disableHotspots": true,
+"pages": [
+$.extend(new ViewerPage(),{
+"index": 0,
+"image": "0._home.png",
+"image2x": "0._home@2x.png",
+"width": 360,
+"height": 640,
+"title": "0. Home",
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 96,
+			"y": 479,
+			"width": 72,
+			"height": 149
+		},
+		"page": 5,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 101,
+			"y": 145,
+			"width": 72,
+			"height": 149
+		},
+		"page": 3,
+		"index": 1
+	},
+	{
+		"rect": {
+			"x": 263,
+			"y": 145,
+			"width": 72,
+			"height": 149
+		},
+		"page": 1,
+		"index": 2
+	},
+	{
+		"rect": {
+			"x": 182,
+			"y": 145,
+			"width": 72,
+			"height": 149
+		},
+		"page": 7,
+		"index": 3
+	},
+	{
+		"rect": {
+			"x": 96,
+			"y": 309,
+			"width": 72,
+			"height": 149
+		},
+		"page": 4,
+		"index": 4
+	},
+	{
+		"rect": {
+			"x": 263,
+			"y": 309,
+			"width": 72,
+			"height": 149
+		},
+		"page": 2,
+		"index": 5
+	},
+	{
+		"rect": {
+			"x": 177,
+			"y": 309,
+			"width": 72,
+			"height": 149
+		},
+		"page": 8,
+		"index": 6
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 1,
+"image": "destacado_1_boton.png",
+"image2x": "destacado_1_boton@2x.png",
+"width": 360,
+"height": 2459,
+"title": "Destacado 1 Boton",
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 360,
+		"height": 24,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "destacado_1_boton-0.png",
+		"image2x": "destacado_1_boton-0@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 587,
+		"width": 360,
+		"height": 56,
+		"type": "float",
+		"index": 1,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "destacado_1_boton-1.png",
+		"image2x": "destacado_1_boton-1@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 643,
+		"width": 360,
+		"height": 1816,
+		"type": "float",
+		"index": 2,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "destacado_1_boton-2.png",
+		"image2x": "destacado_1_boton-2@2x.png"
+	}
+],
+'links' : [
+	{
+		"rect": {
+			"x": 0,
+			"y": 24,
+			"width": 56,
+			"height": 56
+		},
+		"action": "back",
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 15.999999999999943,
+			"y": 389,
+			"width": 328,
+			"height": 42
+		},
+		"url": "https://joseglasolx.github.io/Simulador-Urban-Credit-/#mobile_landing_paso_1",
+		"index": 1
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 2,
+"image": "no_destacado_1_boton.png",
+"image2x": "no_destacado_1_boton@2x.png",
+"width": 360,
+"height": 2459,
+"title": "NO Destacado 1  Boton",
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 360,
+		"height": 24,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "no_destacado_1_boton-0.png",
+		"image2x": "no_destacado_1_boton-0@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 587,
+		"width": 360,
+		"height": 56,
+		"type": "float",
+		"index": 1,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "no_destacado_1_boton-1.png",
+		"image2x": "no_destacado_1_boton-1@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 643,
+		"width": 360,
+		"height": 1816,
+		"type": "float",
+		"index": 2,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "no_destacado_1_boton-2.png",
+		"image2x": "no_destacado_1_boton-2@2x.png"
+	}
+],
+'links' : [
+	{
+		"rect": {
+			"x": 0,
+			"y": 24,
+			"width": 56,
+			"height": 56
+		},
+		"action": "back",
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 15.999999999999972,
+			"y": 389,
+			"width": 328,
+			"height": 42
+		},
+		"url": "https://joseglasolx.github.io/Simulador-Urban-Credit-/#mobile_landing_paso_1",
+		"index": 1
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 3,
+"image": "destacado_2+_botones.png",
+"image2x": "destacado_2+_botones@2x.png",
+"width": 360,
+"height": 2459,
+"title": "Destacado 2+ Botones",
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 360,
+		"height": 24,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "destacado_2+_botones-0.png",
+		"image2x": "destacado_2+_botones-0@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 587,
+		"width": 360,
+		"height": 56,
+		"type": "float",
+		"index": 1,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "destacado_2+_botones-1.png",
+		"image2x": "destacado_2+_botones-1@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 643,
+		"width": 360,
+		"height": 1816,
+		"type": "float",
+		"index": 2,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "destacado_2+_botones-2.png",
+		"image2x": "destacado_2+_botones-2@2x.png"
+	}
+],
+'links' : [
+	{
+		"rect": {
+			"x": 0,
+			"y": 24,
+			"width": 56,
+			"height": 56
+		},
+		"action": "back",
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 15.999999999999943,
+			"y": 389,
+			"width": 328,
+			"height": 42
+		},
+		"page": 6,
+		"index": 1
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 4,
+"image": "no_destacado_2+_botones.png",
+"image2x": "no_destacado_2+_botones@2x.png",
+"width": 360,
+"height": 2459,
+"title": "NO Destacado 2+ Botones",
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 360,
+		"height": 24,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "no_destacado_2+_botones-0.png",
+		"image2x": "no_destacado_2+_botones-0@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 587,
+		"width": 360,
+		"height": 56,
+		"type": "float",
+		"index": 1,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "no_destacado_2+_botones-1.png",
+		"image2x": "no_destacado_2+_botones-1@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 643,
+		"width": 360,
+		"height": 1816,
+		"type": "float",
+		"index": 2,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "no_destacado_2+_botones-2.png",
+		"image2x": "no_destacado_2+_botones-2@2x.png"
+	}
+],
+'links' : [
+	{
+		"rect": {
+			"x": 0,
+			"y": 24,
+			"width": 56,
+			"height": 56
+		},
+		"action": "back",
+		"index": 0
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 5,
+"image": "fixed.png",
+"image2x": "fixed@2x.png",
+"width": 360,
+"height": 2208,
+"title": "Fixed",
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 643,
+		"width": 360,
+		"height": 1565,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "fixed-0.png",
+		"image2x": "fixed-0@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 587,
+		"width": 360,
+		"height": 56,
+		"type": "float",
+		"index": 1,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "fixed-1.png",
+		"image2x": "fixed-1@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 23,
+		"width": 360,
+		"height": 56,
+		"type": "float",
+		"index": 2,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [
+			{
+				"rect": {
+					"x": 0,
+					"y": 0,
+					"width": 161,
+					"height": 56
+				},
+				"action": "back",
+				"index": 0
+			}
+		],
+		"image": "fixed-2.png",
+		"image2x": "fixed-2@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 360,
+		"height": 24,
+		"type": "float",
+		"index": 3,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "fixed-3.png",
+		"image2x": "fixed-3@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 79,
+		"width": 360,
+		"height": 80,
+		"type": "float",
+		"index": 4,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [
+			{
+				"rect": {
+					"x": 0,
+					"y": 0,
+					"width": 360,
+					"height": 80
+				},
+				"page": 6,
+				"index": 1
+			}
+		],
+		"image": "fixed-4.png",
+		"image2x": "fixed-4@2x.png"
+	}
+],
+'links' : [],
+})
+,$.extend(new ViewerPage(),{
+"index": 6,
+"image": "doalog.png",
+"image2x": "doalog@2x.png",
+"width": 360,
+"height": 640,
+"title": "DOALOG",
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 587,
+		"width": 360,
+		"height": 56,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "doalog-0.png",
+		"image2x": "doalog-0@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 360,
+		"height": 24,
+		"type": "float",
+		"index": 1,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "doalog-1.png",
+		"image2x": "doalog-1@2x.png"
+	}
+],
+'links' : [
+	{
+		"rect": {
+			"x": 48,
+			"y": 274,
+			"width": 264,
+			"height": 56
+		},
+		"url": "https://joseglasolx.github.io/Silmulador_UrbanCredit/#mobile_landing_paso_1",
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 48,
+			"y": 330,
+			"width": 264,
+			"height": 56
+		},
+		"url": "https://www.olx.com.ar/nf/landings/cars?from=hero_buttons",
+		"index": 1
+	},
+	{
+		"rect": {
+			"x": 48,
+			"y": 386,
+			"width": 264,
+			"height": 56
+		},
+		"url": "https://www.olx.com.ar/nf/landings/cars?from=hero_buttons",
+		"index": 2
+	},
+	{
+		"rect": {
+			"x": 112,
+			"y": 466,
+			"width": 136,
+			"height": 56
+		},
+		"page": 0,
+		"index": 3
+	},
+	{
+		"rect": {
+			"x": 279,
+			"y": 127,
+			"width": 42,
+			"height": 42
+		},
+		"page": 0,
+		"index": 4
+	},
+	{
+		"rect": {
+			"x": 0,
+			"y": 24,
+			"width": 360,
+			"height": 95
+		},
+		"action": "back",
+		"index": 5
+	},
+	{
+		"rect": {
+			"x": 0,
+			"y": 522,
+			"width": 360,
+			"height": 118
+		},
+		"action": "back",
+		"index": 6
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 7,
+"image": "destacado.png",
+"image2x": "destacado@2x.png",
+"width": 360,
+"height": 2333,
+"title": "Destacado",
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 360,
+		"height": 24,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "destacado-0.png",
+		"image2x": "destacado-0@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 587,
+		"width": 360,
+		"height": 56,
+		"type": "float",
+		"index": 1,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "destacado-1.png",
+		"image2x": "destacado-1@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 643,
+		"width": 360,
+		"height": 1690,
+		"type": "float",
+		"index": 2,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "destacado-2.png",
+		"image2x": "destacado-2@2x.png"
+	}
+],
+'links' : [
+	{
+		"rect": {
+			"x": 0,
+			"y": 24,
+			"width": 56,
+			"height": 56
+		},
+		"action": "back",
+		"index": 0
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 8,
+"image": "no_destacado.png",
+"image2x": "no_destacado@2x.png",
+"width": 360,
+"height": 2333,
+"title": "NO Destacado",
+'type': 'regular',
+'fixedPanels': [
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 0,
+		"width": 360,
+		"height": 24,
+		"type": "float",
+		"index": 0,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "no_destacado-0.png",
+		"image2x": "no_destacado-0@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 587,
+		"width": 360,
+		"height": 56,
+		"type": "float",
+		"index": 1,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "no_destacado-1.png",
+		"image2x": "no_destacado-1@2x.png"
+	},
+	{
+		"constrains": {
+			"top": false,
+			"bottom": false,
+			"left": false,
+			"right": false,
+			"height": false,
+			"width": false
+		},
+		"x": 0,
+		"y": 643,
+		"width": 360,
+		"height": 1690,
+		"type": "float",
+		"index": 2,
+		"isFloat": true,
+		"isFixedDiv": false,
+		"divID": "",
+		"links": [],
+		"image": "no_destacado-2.png",
+		"image2x": "no_destacado-2@2x.png"
+	}
+],
+'links' : [
+	{
+		"rect": {
+			"x": 0,
+			"y": 24,
+			"width": 56,
+			"height": 56
+		},
+		"action": "back",
+		"index": 0
+	}
+],
+})
+   ]
+,"resolutions": [2],
+"title": "Ad_details",
+"totalImages": 34,
+"highlightLinks": false
+}
